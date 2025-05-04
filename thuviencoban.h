e6b367996000503fc89a0b18c1a561f4d3674e74
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-05-04 09:58:07 +0700

    Add files via upload

---
+++ thuviencoban.h
@@ -0,0 +1,26 @@
+/* 1 VÀI THƯ VIỆN CƠ BẢN TRONG BITS/STDC++.H */
+
+// Thư viện nhập xuất cơ bản
+#include <iostream>
+
+// Thư viện string
+#include <string>
+#include <cstring>
+
+// Thư viện thao tác với vector, set, map...
+#include <vector>
+#include <set>
+#include <map>
+
+// Thư viện thuật toán
+#include <algorithm>
+
+// Thư viện toán học
+#include <cmath>
+#include <cstdlib>
+
+// Thư viện iomanip (định dạng in sau dấu chấm)
+#include <iomanip>
+
+using namespace std;
+
